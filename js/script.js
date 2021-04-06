5d505bdc1c5129427367813084599509a26660f4
KelBornn <bryanauthiat1@gmail.com>
2021-04-06 18:43:10 +0200

    oui

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+function bonsoir() {
+    var textWrapper = document.querySelector('.ml7 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml7 .letter',
+    translateY: ["1.1em", 0],
+    translateX: ["0.55em", 0],
+    translateZ: 0,
+    rotateZ: [180, 0],
+    duration: 2000,
+    easing: "easeOutExpo",
+    delay: function(el, i) {
+      return 4000+ 50 * i;
+    }
+  });
+}
+
+
